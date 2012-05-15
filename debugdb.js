7890838c94fa57b877437027f4b3dd788619eac5
Lucas Gonze (acct for Heroku dev) <lucasgonze@mytakedownrequests.com>
2012-05-15 13:18:31 -0700

    asdf

--- debugdb.js
+++ debugdb.js
@@ -1,4 +1,37 @@
+var D = new Date();
 console.log("bp 1");
+console.log(D.getHours());
+console.log(D.getMinutes());
+
+var pg = require('pg'); //native libpq bindings = `var pg = require('pg').native`
+var conString = process.env.DATABASE_URL;
+
+var client = new pg.Client(conString);
+client.connect();
+
+/*
+
+//queries are queued and executed one after another once the connection becomes available
+client.query("CREATE TEMP user (id serial,emailConfirmed bool default false,email text not null,password text not null)");
+client.query("INSERT INTO user(email, password) values($1, $2)", ['test1@example.com', "password 1"]);
+client.query("INSERT INTO user(email, password) values($1, $2)", ['test2@example.com', "password 2"]);
+
+var query = client.query("SELECT * FROM user");
+
+//can stream row results back 1 at a time
+query.on('row', function(row) {
+	console.log("got row");
+  	console.log(row.email);
+  	console.log(row.password);
+});
+
+//fired after last row is emitted
+query.on('end', function() { 
+  client.end();
+});
+*/
+
+// Below is to supress bogus errors in Heroku logs
 
 var sys = require('util');
 var http = require('http');
@@ -18,34 +51,6 @@ var server = http.createServer(function(req, res) {
       callback(queryResult)
     }
   }
-
-	var pg = require('pg'); //native libpq bindings = `var pg = require('pg').native`
-	var conString = process.env.DATABASE_URL;
-
-	var client = new pg.Client(conString);
-	client.connect();
-
-	/*
-
-	//queries are queued and executed one after another once the connection becomes available
-	client.query("CREATE TEMP user (id serial,emailConfirmed bool default false,email text not null,password text not null)");
-	client.query("INSERT INTO user(email, password) values($1, $2)", ['test1@example.com', "password 1"]);
-	client.query("INSERT INTO user(email, password) values($1, $2)", ['test2@example.com', "password 2"]);
-
-	var query = client.query("SELECT * FROM user");
-
-	//can stream row results back 1 at a time
-	query.on('row', function(row) {
-		console.log("got row");
-	  	console.log(row.email);
-	  	console.log(row.password);
-	});
-
-	//fired after last row is emitted
-	query.on('end', function() { 
-	  client.end();
-	});
-*/
 
 });
 
