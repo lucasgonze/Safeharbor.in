ea90890493e5507af10cd601093531b8892bafcc
Lucas Gonze (acct for Heroku dev) <lucasgonze@mytakedownrequests.com>
2012-05-15 11:53:22 -0700

    asdf

--- db.js
+++ db.js
@@ -7,7 +7,9 @@ console.log("bp 1");
 // https://github.com/brianc/node-postgres
 var pg = require('pg');
 
-function showUserTable(){
+function handler(err,result){
+    //err is the error returned from the PostgreSQL server
+    //handle the error here
 	
 }
 
@@ -20,13 +22,28 @@ function createUserTable() {
     client.connect();
     client.query("drop table if exists user");
 
-	client.query("create table user (id serial,emailConfirmed bool default false,email text not null,password text not null)");
+	client.query("create table user (id serial,emailConfirmed bool default false,email text not null,password text not null)",
+		function (err,result){
+			console.log("got error on create table");
+			console.log(err);
+			console.log("===>")
+			console.log(result);
+			console.log("<===")
+		}		
+	);
 
 	client.query({
 	  name: 'insert user',
 	  text: "INSERT INTO user(email,password) values($1, $2)",
 	  values: ["email 1","1234"]
-	});
+	},
+		function (err,result){
+			console.log("got error on insert");
+			console.log(err);
+			console.log("===>")
+			console.log(result);
+			console.log("<===")
+		});
 	client.query({
 	  name: 'insert user',
 	  text: "INSERT INTO user(email,password) values($1, $2)",
@@ -38,7 +55,14 @@ function createUserTable() {
 	  values: ["email 2","abcd"]
 	});
 
-	var selectQuery = client.query("SELECT * FROM user");
+	var selectQuery = client.query("SELECT * FROM user",
+		function (err,result){
+			console.log("got error on select *");
+			console.log(err);
+			console.log("===>")
+			console.log(result);
+			console.log("<===")
+		});
 
     console.log("2.a");
 
