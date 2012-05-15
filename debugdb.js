7ab065667e2957b79af7644b12190b3d1cf65320
Lucas Gonze (acct for Heroku dev) <lucasgonze@mytakedownrequests.com>
2012-05-15 12:44:45 -0700

    asdf

--- debugdb.js
+++ debugdb.js
@@ -5,31 +5,17 @@ var client = new pg.Client(conString);
 client.connect();
 
 //queries are queued and executed one after another once the connection becomes available
-client.query("CREATE TEMP TABLE beatles(name varchar(10), height integer, birthday timestamptz)");
-client.query("INSERT INTO beatles(name, height, birthday) values($1, $2, $3)", ['Ringo', 67, new Date(1945, 11, 2)]);
-client.query("INSERT INTO beatles(name, height, birthday) values($1, $2, $3)", ['John', 68, new Date(1944, 10, 13)]);
+client.query("CREATE TEMP user (id serial,emailConfirmed bool default false,email text not null,password text not null)");
+client.query("INSERT INTO user(email, password) values($1, $2)", ['test1@example.com', "password 1"]);
+client.query("INSERT INTO user(email, password) values($1, $2)", ['test2@example.com', "password 2"]);
 
-//queries can be executed either via text/parameter values passed as individual arguments
-//or by passing an options object containing text, (optional) parameter values, and (optional) query name
-client.query({
-  name: 'insert beatle',
-  text: "INSERT INTO beatles(name, height, birthday) values($1, $2, $3)",
-  values: ['George', 70, new Date(1946, 02, 14)]
-});
-
-//subsequent queries with the same name will be executed without re-parsing the query plan by postgres
-client.query({
-  name: 'insert beatle',
-  values: ['Paul', 63, new Date(1945, 04, 03)]
-});
-var query = client.query("SELECT * FROM beatles WHERE name = $1", ['John']);
+var query = client.query("SELECT * FROM user");
 
 //can stream row results back 1 at a time
 query.on('row', function(row) {
-  console.log(row);
-  console.log("Beatle name: %s", row.name); //Beatle name: John
-  console.log("Beatle birth year: %d", row.birthday.getYear()); //dates are returned as javascript dates
-  console.log("Beatle height: %d' %d\"", Math.floor(row.height/12), row.height%12); //integers are returned as javascript ints
+	console.log("got row");
+  	console.log(row.email);
+  	console.log(row.password);
 });
 
 //fired after last row is emitted
