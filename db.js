c3c4f6776efcbdf35175bb791d53feaea00d6371
Lucas Gonze (acct for Heroku dev) <lucasgonze@mytakedownrequests.com>
2012-05-15 11:35:25 -0700

    asdf

--- db.js
+++ db.js
@@ -7,6 +7,10 @@ console.log("bp 1");
 // https://github.com/brianc/node-postgres
 var pg = require('pg');
 
+function showUserTable(){
+	
+}
+
 function createUserTable() {
 
 	console.log("bp 2");
@@ -15,27 +19,26 @@ function createUserTable() {
     client.connect();
     client.query("drop table if exists user");
 
-    /*
-client.query("create table user (id serial,emailConfirmed bool default false,email text not null,password text not null)");
+	client.query("create table user (id serial,emailConfirmed bool default false,email text not null,password text not null)");
 
-client.query({
-  name: 'insert user',
-  text: "INSERT INTO user(email,password) values($1, $2, $3)",
-  values: ["test@example.com","1234"]
-});
+	client.query({
+	  name: 'insert user',
+	  text: "INSERT INTO user(email,password) values($1, $2, $3)",
+	  values: ["test@example.com","1234"]
+	});
 
-var selectQuery = client.query("SELECT * FROM user");
+	var selectQuery = client.query("SELECT * FROM user");
 
-//can stream row results back 1 at a time
-query.on('row', function(row) {
-  console.log(row);
-});
+	//can stream row results back 1 at a time
+	selectQuery.on('row', function(row) {
+	  console.log(row);
+	});
 
-//fired after last row is emitted
-query.on('end', function() { 
-  client.end();
-});
-*/
+	//fired after last row is emitted
+	query.on('end', function() { 
+	  client.end();
+	});
+	
     client.end();
 	console.log("bp 3");
 
