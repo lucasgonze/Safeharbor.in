572e1a906fc4ce18be4c7f418b2bce3d82ce3d94
Victor Stone <victor.stone@disney.com>
2012-06-20 18:53:26 -0400

    newer code

--- models/dash-models.js
+++ models/dash-models.js
@@ -48,19 +48,58 @@ exports.logTakeDownRequest = function( obj, callback )
     return appender.chain( requestInfo );
 }
 
-exports.getAuditLog = function( acctid, callback )
+exports.getAuditLog = function( uid, callback )
 {
     // TODO: add paging
     
     var sql = 'SELECT *, ' +
-              "to_char(creation, 'FMMonth FMDD, YYYY' ) as formatted_date" +
+              "to_char(creation, 'FMMonth FMDD, YYYY' ) as formatted_date " +
               'FROM audit, site, acct ' +
               'WHERE audit.siteid = site.siteid ' +
               '  AND site.ownerid = acct.acctid ' +
-              '  AND acct.acctid = $1' +
-              'ORDER creation DESC';
+              '  AND acct.acctid = $1 ' +
+              'ORDER BY creation DESC ';
               
-    return new ModelPerformer( { values: [acctid],
-                             callback: callback,
-                             perform: function() { this.table.findAllRecords(sql); } });
+    var AllRows = new ModelPerformer( { values: [uid],
+                             performer: function() { this.table.findAllRows(sql); },
+                             callback: function( code, rows ) 
+                                 {
+                                    if( code == models.CODES.SUCCESS )
+                                        this.allRows = rows;
+                                    else
+                                        this.callback(code,rows);
+                                 },
+                             });
+
+    var sql2 = 'SELECT * FROM requests WHERE trackingid = $1';                             
+    var subSqlPerformer = new ModelPerformer( { 
+                                    performer: function() 
+                                    {
+                                        var rows = this.findValue('allRows');
+                                        var me = this, len = rows.length;
+                                        function indexCallback(index,row) {
+                                            return function(code, trRows) {
+                                                    if( code == models.CODES.SUCCESS )
+                                                    {
+                                                        code = models.CODES.QUERY_ROW;
+                                                        row.takedownRequests = trRows;
+                                                    }
+                                                    me.callback(code,trRows,row);
+                                                    // TODO: this should be a counter up to 'len'
+                                                    if( index == len - 1 )
+                                                        me.callback(models.CODES.SUCCESS,rows);
+                                                }  
+                                        }
+                                
+                                        for( var i = 0; i < len; i++ )
+                                        {
+                                            this.table.findAllRows(sql2, [rows[i].auditid], indexCallback(i,rows[i]) );
+                                        }
+                                        
+                                    },
+                                    callback: callback
+                                    });
+                                    
+    return AllRows.chain( subSqlPerformer );
+
 }
