84ee7fa578a4eca57f3e1bafa6806a8e0fe150b3
Victor Stone <victor.stone@disney.com>
2012-06-21 18:23:16 -0400

    missed the template file

--- routes/dash-routes.js
+++ routes/dash-routes.js
@@ -12,12 +12,30 @@ var checkForSQLErr = errlib.errout( [ models.CODES.SQL_ERROR ] );
 /* stub to fill in later */
 function getDisputes(req,res)
 {
-    res.render( '../views/disputes/all.html',
-                {
-                   layout: 'signedin.html',
-                   pageTitle: 'Safe Harbor - Disputes',
-                   bodyClass: 'disputes'
-                } );
+    var uid = 1; // loginstate.getID(req);
+    
+    if( !uid ) {
+	    errout( req, res, exp( 400, "Only somebody signed in can see site info." ) );
+        return(false);
+    }
+
+    var log = models.getAuditLog(uid,function(code,rows) 
+                    {
+                        checkForSQLErr( req, res, code, rows );
+                        if( code == models.CODES.SUCCESS )
+                        {
+                            res.render( '../views/disputes/all.html',
+                                        {
+                                           layout: 'signedin.html',
+                                           pageTitle: 'Safe Harbor - Disputes',
+                                           bodyClass: 'disputes',
+                                           body: require('util').inspect(rows,true,null)
+                                        } );
+                        }
+                    });
+    
+    log.perform();
+
 }
 
 function getDash( req, res )
