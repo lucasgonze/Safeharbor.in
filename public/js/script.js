c3d0de42acac7ab5cfad3e98364140202a737848
Lucas Gonze <lucas@gonze.com>
2012-08-07 18:28:50 -0700

    Integrate Paige's styling changes to box.css, her new lighthouse.png background image for the inbox, her new PSD for that image, her new favicon.
    Deliver the day's worth of work on the demo, which is the cause of all other edits in this commit.

--- public/js/script.js
+++ public/js/script.js
@@ -191,103 +191,140 @@ function initAjaxForms(){
 
 function initTour(){
 	
-	if( window.location.pathname !== "/demo" && window.location.search !== "?tour" )
+	if( window.location.pathname !== "/demo" && window.location.search.match("autopilot=on") === null )
 		return;
 
-/* Fixme: 
-
-Get [showControls:true] working
-Fix the bug transitioning to a new URL
-
-*/
-
-	var tourdata = [
-	{
-		html: "Please set your browser to full-screen mode.",
-		expose: true,
-		element: "div#demo10",
-		goTo: null
-	}
-	, {
-		html: 'Imagine a web site that hosts content uploaded by its user community. As an example we will use <a href="http://etsy.com">Etsy</a>.',
-		expose: true,
-		element: "div#demo20",
-		goTo: null
-	}
-	, {
-		html: "The site has posted a link for copyright complaints.",
-		expose: true,
-		element: "div#demo30"
-	}
-	, {
-		html: "When the link is activated, it goes to a page of legalese.",
-		expose: true,
-		element: "div#demo40"
-	}
-	, {
-		html: "Our product replaces the legalese.",
-		expose: true,
-		element: "div#demo50",
-		goTo: "/box/3416a75f4cea9109507cacd8e2f2aefc?tour",
-	}
-	, {
-		html: "Human-friendly navigation",
-		expose: true,
-		element: "div#splash",
-		position: "n"
-	}
-	, {
-		html: "Education",
-		expose: true,
-		element: "a#learnmorebutton",
-		position: "n",
-		goTo: "/box/help/learn/3416a75f4cea9109507cacd8e2f2aefc?tour"
-	}
-	, {	
-		overlayOpacity: false,
-		element: "li#Q7 h4",
-		position: "e",
-		expose: true,
-		html: "Prevent complaints that aren't about copyright."
-	}
-	, {		
-		element: "li#Q5 h4",
-		overlayOpacity: false,
-		html: "Pre-written answers to standard questions",
-		expose: true,
-		position: "e",
-		goTo: "/box/3416a75f4cea9109507cacd8e2f2aefc?tour"
-	}, {
-		element: "img.sitelogo",
-		expose: true,
-		position: "e",
-		html: "Easy customization: just set logo."
-	}, {
-		element: "#haslowerborder a",
-		expose: true,
-		position: "s",
-		html: "Prevent frivolous complaints by setting serious tone."
-	}, {
-		"html": "And all this is BEFORE the main act",
-		element: "a#submitbutton",
-		position: "n",
-		goTo: "/box/role/3416a75f4cea9109507cacd8e2f2aefc?tour"
-	}, {
-		html: "Focus newbies attention when necessary",
-		live: 100000,
-		element: "#Q4",
-		goTo: "/box/form/3416a75f4cea9109507cacd8e2f2aefc?tour&person-type=co"
-	}, {
-		expose: true,
-		html: "Usability instead of legalese"
-	}
-		
-	
+	var tourdata = [	
+		{
+			element: "div#demo0",
+			live: 5000,
+			html: "Welcome",
+			animationIn: 'fadeIn',
+			animationOut: 'fadeOut',
+			position: "n"
+		}, 
+		{
+			html: "Click the caption block to pause, stop, or skip.",
+			element: "div#demo0",
+			position: "n"
+		}, 
+		{
+			html: "Please set your browser to full-screen mode.",
+			element: "div#demo10 img.tgt",
+			position: "nw",
+			live: 10000,
+			goTo: null
+		}
+		, {
+			html: 'Imagine a web site that hosts content uploaded by its user community. As an example we will use <a href="http://etsy.com">Etsy</a>.',
+			element: "div#demo20",
+			goTo: null
+		}
+		, {
+			html: "The site has posted a link for copyright complaints.",
+			element: "div#demo30"
+		}
+		, {
+			html: "When the site's link is activated, it goes to a page of static legal text.",
+			position: "w",
+			element: "div#demo40 img"
+		}
+		, {
+			html: "Our web app replaces the text.",
+			element: "div#demo50 div.show",
+			position: "w",
+			goTo: "/box/3416a75f4cea9109507cacd8e2f2aefc?autopilot=on",
+		}
+		, {
+			html: "Human-friendly navigation",
+			element: "div#splash",
+			position: "n"
+		}
+		, {
+			html: "Education",
+			element: "a#learnmorebutton",
+			position: "n",
+			goTo: "/box/help/learn/3416a75f4cea9109507cacd8e2f2aefc?autopilot=on"
+		}
+		, {	
+			overlayOpacity: false,
+			element: "li#Q7 h4",
+			position: "e",
+			html: "Prevent complaints that aren't about copyright."
+		}
+		, {		
+			element: "li#Q5 h4",
+			overlayOpacity: false,
+			html: "Pre-written answers to standard questions.",
+			position: "e",
+			goTo: "/box/3416a75f4cea9109507cacd8e2f2aefc?autopilot=on"
+		}, {
+			element: "img.sitelogo",
+			position: "e",
+			html: "Easy customization: just set logo."
+		}, {
+			element: "#haslowerborder a",
+			position: "s",
+			html: "Prevent frivolous complaints by setting serious tone."
+		}, {
+			html: "And all this is BEFORE the main act.",
+			element: "a#submitbutton",
+			position: "n",
+			goTo: "/box/role/3416a75f4cea9109507cacd8e2f2aefc?autopilot=on"
+		}, {
+			html: "Prevent common causes of bogus complaints.",
+			element: "a#authorizedagent",
+			goTo: "/box/form/3416a75f4cea9109507cacd8e2f2aefc?autopilot=on&person-type=co"
+		}, {
+			html: "Usability instead of legalese"
+		}, {
+			element: "input#anchor",
+			position: "e",
+			html: "Make it easy to figure out what the notification is in regard to."
+		}, {
+			element: "button#report_another",
+			position: "e",
+			html: "Multiple notifications per submission: less work and fewer mistakes."
+		}, {
+			element: "div#legalrequiredfields",
+			position: "n",
+			html: "Big type makes copyright owners relaxed instead of mad."
+		}, {
+			element: "div.row.bottom",
+			position: "n",
+			html: "A copyright owner must be able to bypass the form, but bypassing the form is more work for the web site. We channel copyright owners into the form by putting the bypass link after the form.",
+			goTo: "/box/bymail/3416a75f4cea9109507cacd8e2f2aefc?autopilot=on"
+		}, {
+			html: "Sites often leave one of these pieces of contact information out, and they are all required by law."
+		}
 	];
+	
+	var debugdata = null;
+	debugdata = [		
+		{
+			html: "debug step 1",
+			live: 1,
+			goTo: "/box/bymail/3416a75f4cea9109507cacd8e2f2aefc?autopilot=on"
+		} 
+		, {
+			html: "Sites often leave one of these pieces of contact information out, and they are all required by law."
+		}		
+	];
+	
+//	tourdata = debugdata;
+
+/*	
+	background opacity down
+	screenshots bigger
+	money shot much more clear, bigger, 
+	*/
+	
 	var myTour = jTour(tourdata,{
 			showControls: true,
 			showProgress: true,
-			autostart: true
+			autostart: true,
+			overlayOpacity: .25,
+			expose: true
 		});
 	myTour.start();
 }
