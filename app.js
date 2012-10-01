473d4ac9dfdd0e18c636462276834279d194b5c6
Lucas Gonze <lucas@gonze.com>
2012-10-01 15:53:28 -0700

    touch Mon Oct  1 15:53:28 PDT 2012

--- app.js
+++ app.js
@@ -8,8 +8,49 @@
 var express = require('express');
 var app = express.createServer();
 
+var globalClient = null;
+var getClient = function() 
+{
+    if( !globalClient )
+    {
+        var pg = require('pg'); //native libpq bindings = `var pg = require('pg').native`
+        var conString = process.env.DATABASE_URL || "tcp://postgres:EMbr4EDS@localhost/safeharborin"; // on heroku and on my local dev box
+        var client = new pg.Client(conString);
+        if( !client )
+            throw new InvalidConnect(conString);
+        client.connect();
+        globalClient = client;
+    }
+	return(globalClient);
+}
+
+
 app.get('/', function(req, res){
-  res.send('Hello World');
+	
+	sql = "create temp table if not exists foo (bar text)";
+    var query = getClient().query( sql, args ); // do NOT pass a callback here
+    
+    if( !query ){
+		console.log("no query");
+		res.send("no query");
+		return;
+    }
+
+    query.on('row', function(data) { 
+		console.log("row: ",data);
+		res.send('Hello World 1');
+	});
+    
+    query.on('end', function(data) { 
+		console.log("end: ",data);
+		res.send('Hello World 2');
+	});
+    
+    query.on('error', function(data) { 
+		console.log("error: ",data);	
+		res.send('Hello World 3');
+	});
+    
 });
 
 // 
